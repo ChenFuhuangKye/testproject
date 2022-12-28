0c6ee5fa6b357da06299f53f30f6522dbbf4aa57
chenfu <kye1234321@gmail.com>
2022-12-28 21:13:41 +0800

    創建資料

---
+++ ConsoleApp1Tests/ServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Tests
+{
+    [TestClass()]
+    public class ServiceTests
+    {
+        [TestMethod()]
+        public void 測試測試功能是否正常()
+        {
+            Assert.AreEqual(1, 1);
+        }
+        [TestMethod()]
+        public void 測試addTest是否正常()
+        {
+            string[] args = { "8", "7" };
+            int result = ConsoleApp1.MyService.add(args);
+            Assert.AreEqual(result, 15);
+        }
+    }
+}
